f62cf3d6a07880c7e85f1f9b1c12398bc03c5417
ManuGave <manu.gave08@gmail.com>
2025-05-24 02:33:57 -0300

    feat update RegistrosN.txt feat update estrategia.c

--- estrategia.c
+++ estrategia.c
@@ -127,16 +127,26 @@ int pruebas_backtracking(Nivel* nivel, Mapa* mapa, Pila* pila, int i){
     int cantidad_de_casillas= mapa->alto*mapa->ancho;
     Coordenada pos_val[cantidad_de_casillas];
     int cant_pos_validas=posiciones_validas(pos_val, mapa->casillas, mapa->alto, mapa->ancho);
+    while(bandera != 1){
     //Si el i es el inicial, osea 0 arranco con la disposicion inicial.Si no voy con las pruebas recursivas.El i =-1 es la cancelacion.
-    if(quedan_opciones_validas){
-    //Verifca que queden opciones validas
-        if(i == 0 && bandera != 1)
-        //Verifica que no sea la inicial o la ultima simulacion.
-            if(simular_nivel(nivel, mapa, disposicion_inicial_backtracking)){
-                printf("Se encontro la disposicion correcta");
-                bandera = 1;
-                return 1;
-            }
+        if(quedan_opciones_validas){
+        //Verifca que queden opciones validas
+            if(i == 0)
+            //Verifica que no sea la inicial o la ultima simulacion.
+                if(simular_nivel(nivel, mapa, disposicion_inicial_backtracking)){
+                    printf("Se encontro la disposicion correcta");
+                    bandera = 1;
+                    return 1;
+                }
+                else{
+                    int pos_x = pos_val[i + mapa->cant_torres].x;
+                    int pos_y = pos_val[i + mapa->cant_torres].y;
+                    pila_desapilar(pila);
+                    colocar_torre(mapa,pos_x,pos_y, i + mapa->cant_torres);
+                    pila_apilar(pila, pos_val[i + mapa->cant_torres]);
+                    i++;
+                    pruebas_backtracking(nivel, mapa, pila, i);
+                    }
             else{
                 int pos_x = pos_val[i + mapa->cant_torres].x;
                 int pos_y = pos_val[i + mapa->cant_torres].y;
@@ -146,24 +156,30 @@ int pruebas_backtracking(Nivel* nivel, Mapa* mapa, Pila* pila, int i){
                 i++;
                 pruebas_backtracking(nivel, mapa, pila, i);
                 }
-        else{
-            int pos_x = pos_val[i + mapa->cant_torres].x;
-            int pos_y = pos_val[i + mapa->cant_torres].y;
-            pila_desapilar(pila);
-            colocar_torre(mapa,pos_x,pos_y, i + mapa->cant_torres);
-            pila_apilar(pila, pos_val[i + mapa->cant_torres]);
-            i++;
-            pruebas_backtracking(nivel, mapa, pila, i);
-            }
-            return 1;
-            //Hasta aca, la disposicion solo backtrackea una opcion.
+                return 1;
+                //Hasta aca, la disposicion solo backtrackea una opcion.
+        }
+        //Con el else lo que hago es que si las opciones se me acabaron, desapilo una, dejo la ultima opcion como valida, apilo la primera opcion, y vuelvo a probar.
+            else
+                return 0;
     }
-    //Con el else lo que hago es que si las opciones se me acabaron, desapilo una, dejo la ultima opcion como valida, apilo la primera opcion, y vuelvo a probar.
-        else
-            return 0;
 }
 
 //Hasta ahora, pruebo una sola linea.Hay que hacer una funcion para que use la funcion de probar una sola linea usando abarcando la posibilidad de tener que desapilar una vez mas.
+//La idea de la funcion bajar_nivel es que funcione independientemente de en que nivel esta.
+
+void bajar_nivel(Nivel* nivel, Mapa* mapa, Pila* pila){
+    if(!pruebas_backtracking(nivel, mapa, pila, 0)){
+        if(pila_es_vacia(pila)){
+            printf("No hay alternativa. No hay forma de ganar");
+        }
+        else
+            pila_desapilar(pila);
+            
+    }
+}
+
+
 
 void disponer_custom(Nivel* nivel, Mapa* mapa) {
     /* A cargo de la/el estudiante */
