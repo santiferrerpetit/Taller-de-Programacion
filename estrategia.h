d551d6045b4e985cddac049c47998d0b28fc9a3d
santiferrerpetit <ferrerpetitsantiago@gmail.com>
2025-05-23 21:04:50 -0300

    v6

--- estrategia.h
+++ estrategia.h
@@ -10,8 +10,8 @@
 typedef void (*DisposicionTorres)(Nivel*, Mapa*);
 
 void disponer(Nivel* nivel, Mapa* mapa);
-
-void disponer_con_backtracking(Nivel* nivel, Mapa* mapa);
+void backtracking(Nivel* nivel, Mapa* mapa);
+void disponer_con_backtracking(Pila* pila, Nivel* nivel, Mapa* mapa);
 
 void disponer_custom(Nivel* nivel, Mapa* mapa);
 
